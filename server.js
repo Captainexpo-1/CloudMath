3a9cf56ed19afa6ae7a7273495d39577f2051842
Elliot <65295823+Captainexpo-1@users.noreply.github.com>
2023-10-31 17:25:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const app = express();
 const port = 5500;
 
 app.get('/', (req, res) => {
+  console.log("requested for homepage")
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/api/math', (req, res) => {
   const q = req.query
-  console.log(q)
+  console.log("recieved query")
   res.send(
     {
       "result": get_op.GetOperation(q).toString()
